7817811e70dfa87eec4662ac11fdf8f4ec062f3f
shaurya jain <sjvn2706@gmail.com>
2025-08-15 14:06:23 +0530

    Update 20240415184209_init.js

--- Backend/migrations/20240415184209_init.js
+++ Backend/migrations/20240415184209_init.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('test-users',(table)=>{
         table.increments('id').primary()
         table.string('username')
-        table.string("email")
+        table.string('email')
         table.string('password')
     })
   
