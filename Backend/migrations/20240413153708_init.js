b5d959f69b6d7f5fb6fba17b81bf68df38f5500c
shaurya jain <sjvn2706@gmail.com>
2025-08-15 13:58:35 +0530

    Update 20240413153708_init.js

--- Backend/migrations/20240413153708_init.js
+++ Backend/migrations/20240413153708_init.js
@@ -21,6 +21,6 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    return knex.schema.dropTable('movie_shows')
+    return knex.schema.dropTable("movie_shows")
   
 };
