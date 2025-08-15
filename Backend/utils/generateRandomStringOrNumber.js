f147c0bffd2028e19a861de348858ee4a67a2ae6
shaurya jain <sjvn2706@gmail.com>
2025-08-15 14:18:51 +0530

    Update generateRandomStringOrNumber.js

--- Backend/utils/generateRandomStringOrNumber.js
+++ Backend/utils/generateRandomStringOrNumber.js
@@ -16,7 +16,7 @@ function generateRandomString() {
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      result= result + characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
   }
